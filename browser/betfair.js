5dfc87f95e4d41d8ec9b67a73ddd9fe207f6adc2
AlgoTrader <anton.zem@gmail.com>
2012-12-14 15:04:52 +0000

    browser added

---
+++ browser/betfair.js
@@ -0,0 +1,36 @@
+// jQuery should be loaded before this script
+// $.post is used from there
+
+function betfair($, key) {
+    var url = "https://beta-api.betfair.com/json-rpc"
+    var appKey = key;
+    var id = 1;
+    
+    function invokeMethod(method, params, cb) {
+        var inv = {
+            "jsonrpc" : "2.0",
+            "id" : id++,
+            "method" : "SportAPING/v1.0/"+method,
+            "params" : params
+        };
+        var ajax = {
+            url: url,
+            headers: {
+                'X-Application' : '123',
+                'X-Authentication' : '123',
+            }
+            type: "POST"
+        };
+        
+        function onDone() {
+            alert("done");
+        }
+        function onFail() {
+            alert("fail");
+        }
+        $.ajax( {
+        }, inv ).done(onDone).fail(onFail);
+    }
+    
+    
+}
