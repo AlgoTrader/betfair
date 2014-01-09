e16b49e2ccb5b8914887b82b63d6252a024120d0
AlgoTrader <anton.zem@gmail.com>
2014-01-09 08:50:22 +0000

    added logging of login/logout/keepAlive

--- test/login.js
+++ test/login.js
@@ -9,7 +9,11 @@ settings.session = betfair.newSession();
 settings.login = process.env['BF_LOGIN'] || "nobody";
 settings.password = process.env['BF_PASSWORD'] || "password";
 
+var session = settings.session;
+session.startInvocationLog({level: 'info', path: 'log_invocations.txt'});
+
 async.series([common.login, common.keepAlive, common.logout], function (err, res) {
     console.log("Done");
     process.exit(0);
 });
+session.startInvocationLog({level: 'info', path: 'log_invocations.txt'});
