3ce85a5ef5fa4c83deae25d4ac458a32914c6642
Paul Grimshaw <paul.grimshaw@quick-tracks.com>
2015-11-24 21:52:05 +0000

    Fixed path error in index.js

--- index.js
+++ index.js
@@ -6,14 +6,14 @@
 
 // Export BetfairSession 
 // Used to invoke Betfair JSON-RPC methods
-var BetfairSession = require('./lib/betfair_session');
+var BetfairSession = require('./src/lib/betfair_session');
 exports.newSession = function (appKey, options) {
     return new BetfairSession(appKey, options);
 };
 
 // Export BetfairPrice
 // Used to "normalize" prices to Betfair allowed values
-var BetfairPrice = require('./lib/betfair_price');
+var BetfairPrice = require('./src/lib/betfair_price');
 exports.newBetfairPrice = function (price) {
     return new BetfairPrice(price);
 }
