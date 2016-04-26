f6077e33b1b305f64e6b5adbb01d1376fe3edbc8
Anton Zemlyanov <azemlyanov@mirantis.com>
2016-04-26 12:07:40 +0300

    added emulator test

---
+++ src/test/emulator/list_market_book.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var async = require('async');
+var betfair = require("../../index.js");
+var common = require('../common.js');
+var _ = require('lodash');
+
+// Create session to Betfair and start log
+var session = common.initialize({emulator: true});
+
+function listMarketBook(cb) {
+    console.log('===== Invoke listMarketBook... =====');
+    var req = {
+        marketIds: [common.settings.selectedMarket.marketId],
+        matchProjection: 'NO_ROLLUP',
+        priceProjection: { priceData: ['EX_BEST_OFFERS', 'EX_TRADED'] }
+    };
+    session.listMarketBook(req, function (err, res) {
+        console.log("listMarketBook err=%s duration=%s", err, res.duration / 1000);
+        console.log("Request:%s\n", JSON.stringify(res.request, null, 2));
+        console.log("Response:%s\n", JSON.stringify(res.response, null, 2));
+        cb(err);
+    });
+}
+
+async.series([common.login, common.listMarketCatalogue, common.selectMarket, listMarketBook, common.logout], function (err, res) {
+    console.log("Done, err =", err);
+    common.exit(0);
+});
